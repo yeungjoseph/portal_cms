34f90dacd046afc544f6dff7eac7e4342f58ac2f
Joseph Yeung <yeungjoseph10@gmail.com>
2018-01-22 16:52:38 -0800

    Upate page model

--- models/page.js
+++ models/page.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema({
 	title: { type: String, required: true},
 	author: { type: Object, required: true},
-	author_id: { type: String, required: true},
 	content: { type: String, required: true},
 	url: { type: String, required: true, unique: true},
 	template: { type: String, required: true}
